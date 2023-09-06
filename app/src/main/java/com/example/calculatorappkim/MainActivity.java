1c07753c2b01a1fd075c6e7076b507838441f336
kijo534 <113717871+kijo534@users.noreply.github.com>
2023-09-06 08:19:27 -0500

    Added all the buttons, no functionality

--- app/src/main/java/com/example/calculatorappkim/MainActivity.java
+++ app/src/main/java/com/example/calculatorappkim/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculatorappkim;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void add(){
+        TextView num1 = findViewById(R.id.num1);
+        TextView num2 = findViewById(R.id.num2);
+        TextView answer = findViewById(R.id.answer);
+        answer.setText(num1 + num2);
     }
 }
