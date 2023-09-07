1ce81678de7644e52ee365f1ee937d2504f9bece
kijo534 <113717871+kijo534@users.noreply.github.com>
2023-09-07 08:20:06 -0500

    made basic functionality, started button calculator enhancements.

--- app/src/main/java/com/example/calculatorappkim/MainActivity.java
+++ app/src/main/java/com/example/calculatorappkim/MainActivity.java
@@ -3,10 +3,11 @@ package com.example.calculatorappkim;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Double previousNum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,10 +15,40 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void add(){
-        TextView num1 = findViewById(R.id.num1);
-        TextView num2 = findViewById(R.id.num2);
-        TextView answer = findViewById(R.id.answer);
-        answer.setText(num1 + num2);
+    public void add(View view){
+        TextView num1TV = findViewById(R.id.num1);
+        double num1 = Double.parseDouble(num1TV.getText().toString());
+        TextView num2TV = findViewById(R.id.num2);
+        double num2 = Double.parseDouble(num2TV.getText().toString());
+        TextView answerTV = findViewById(R.id.answer);
+        String answer = String.valueOf((num1 + num2));
+        answerTV.setText(answer);
+    }
+    public void multiply(View view){
+        TextView num1TV = findViewById(R.id.num1);
+        double num1 = Double.parseDouble(num1TV.getText().toString());
+        TextView num2TV = findViewById(R.id.num2);
+        double num2 = Double.parseDouble(num2TV.getText().toString());
+        TextView answerTV = findViewById(R.id.answer);
+        String answer = String.valueOf((num1 * num2));
+        answerTV.setText(answer);
+    }
+    public void subtract(View view){
+        TextView num1TV = findViewById(R.id.num1);
+        double num1 = Double.parseDouble(num1TV.getText().toString());
+        TextView num2TV = findViewById(R.id.num2);
+        double num2 = Double.parseDouble(num2TV.getText().toString());
+        TextView answerTV = findViewById(R.id.answer);
+        String answer = String.valueOf((num1 - num2));
+        answerTV.setText(answer);
+    }
+    public void divide(View view){
+        TextView num1TV = findViewById(R.id.num1);
+        double num1 = Double.parseDouble(num1TV.getText().toString());
+        TextView num2TV = findViewById(R.id.num2);
+        double num2 = Double.parseDouble(num2TV.getText().toString());
+        TextView answerTV = findViewById(R.id.answer);
+        String answer = String.valueOf((num1 / num2));
+        answerTV.setText(answer);
     }
 }
